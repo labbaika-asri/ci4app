5d6cb18568121977cc8477aecac8bbbef8036c6d
Labbaika Asri <labbaikaasri07@gmail.com>
2020-09-20 22:36:17 +0700

    Membuat fitur insert serta validasi

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -42,5 +42,7 @@ class BaseController extends Controller
         // E.g.:
         // $this->session = \Config\Services::session();
         // $this->komikModel = new \App\Models\KomikModel();
+
+        session();
     }
 }
--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -41,7 +41,8 @@ class Komik extends BaseController
     public function create()
     {
         $data = [
-            "title" => "From tambah data komik"
+            "title" => "From tambah data komik",
+            "validation" => \Config\Services::validation(),
         ];
 
         return view('komik/create', $data);
@@ -49,8 +50,21 @@ class Komik extends BaseController
 
     public function save()
     {
-        $slug = url_title($this->request->getVar('judul'), '-', true);
+        if (!$this->validate([
+            // 'judul' => 'required|is_unique[komik.judul]',
+            'judul' => [
+                'rules' => 'required|is_unique[komik.judul]',
+                'errors' => [
+                    'required' => '{field} komik harus diisi.',
+                    'is_unique' => '{field} komik sudah terdaftar.'
+                ]
+            ],
+        ])) {
+            $validation = \Config\Services::validation();
+            return redirect()->to('/komik/create')->withInput()->with('validation', $validation);
+        };
 
+        $slug = url_title($this->request->getVar('judul'), '-', true);
         $this->komikModel->save([
             'judul' => $this->request->getVar('judul'),
             'slug' => $slug,
