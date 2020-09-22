64852b9d19a4e40d10d35f54c6e058e3ea71e140
Labbaika Asri <labbaikaasri07@gmail.com>
2020-09-22 15:59:00 +0700

    Migration, Seeder and Faker

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,30 @@
+<?php namespace App\Database\Seeds;
+
+use CodeIgniter\I18n\Time;
+
+class OrangSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        for ($i=0; $i < 150; $i++) {
+            $faker = \Faker\Factory::create('id_ID');
+            $data = [
+                'nama'       => $faker->name,
+                'alamat'     => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at' => Time::now(),
+            ];
+            $this->db->table('orang')->insert($data);
+        }
+
+        // Simple Queries
+        // $this->db->query(
+        //     "INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)",
+        //     $data
+        // );
+
+        // Using Query Builder
+        // $this->db->table('orang')->insert($data);
+        // $this->db->table('orang')->insertBatch($data);
+    }
+}
